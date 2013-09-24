e3acc539007c219ddbb6b1418c56a06ef0a7b6c9
Will Strinz <wstrinz@gmail.com>
2013-09-24 12:44:44 -0500

    add support score for spotlight

--- lib/built_in_modules/dbpedia_spotlight.rb
+++ lib/built_in_modules/dbpedia_spotlight.rb
@@ -17,6 +17,7 @@ class DBPediaSpotlight < Cogibara::Module
         new_prop << [RDF.type, onto_class.SpotlightEntity]
         new_prop << [onto_prop.spotlight_entity_uri, ent["@URI"]]
         new_prop << [onto_prop.spotlight_types, ent["@types"]]
+        new_prop << [onto_prop.spotlight_support_score, ent["@support"]]
         new_prop << [onto_prop.spotlight_surface_form, ent["@surfaceForm"]]
         new_prop
       end
@@ -28,9 +29,13 @@ class DBPediaSpotlight < Cogibara::Module
     structs.map{|st| st.values}.join(', ')
   end
 
-  on(/.*spotlight URIs for "(.+)"$/) do |msg|
+  on(/.*spotlight (URIs|annotations) for "(.+)"$/i) do |annot, msg|
     structs = spotlight_structures(msg)
-    Hash[structs.map{|st| [st.values["spotlight_surface_form"], st.values["spotlight_entity_uri"]]}].to_s
+    if structs
+      Hash[structs.map{|st| [st.values["spotlight_surface_form"], st.values["spotlight_entity_uri"]]}].to_s
+    else
+      "Couldn't find any spotlight annotations for #{msg}"
+    end
   end
 
   on do
