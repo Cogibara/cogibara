453ba9bf3954597504647b7525f89faf3201acf3
Will Strinz <wstrinz@gmail.com>
2013-09-29 11:16:57 -0500

    use gcal for reminders, store wit entities as structs

--- lib/built_in_modules/wit.rb
+++ lib/built_in_modules/wit.rb
@@ -1,9 +1,32 @@
 class Wit < Cogibara::Module
+  include Cogibara::Onto
   requires 'httparty'
   requires 'json'
   requires 'cgi'
 
   requires_key 'wit'
+
+  def entity_hash(json)
+    if json["outcome"] && json["outcome"]["entities"]
+      json["outcome"]["entities"]
+    else
+      {}
+    end
+  end
+
+  def entity_structs(json)
+    entity_hash(json).map{|id,info|
+      new_prop = current_message.new_property
+      new_prop << [RDF.type, onto_class.WitEntity]
+      new_prop << [onto_prop.wit_entity_type, id]
+      info = info.first if info.is_a? Array
+      new_prop << [onto_prop.wit_entity_value, info["value"]]
+      new_prop << [onto_prop.wit_entity_start, info["start"]]
+      new_prop << [onto_prop.wit_entity_end, info["end"]]
+      new_prop << [onto_prop.wit_entity_body, info["body"]]
+      new_prop
+    }
+  end
 
   on(/wit debug "(.+)"/i) do |msg, message|
 
@@ -22,6 +45,7 @@ class Wit < Cogibara::Module
 
     js = JSON.parse(response.body)
     current_message.set_wit_intent(js["outcome"]["intent"]) if js["outcome"] and js["outcome"]["intent"]
+    entity_structs(js).each{|s| current_message << s}
 
     pass
   end
